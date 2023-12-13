9a7f1b1c4bc3196de4c9373905e579d469ddd73e
Mayko Kioschi <mkioschi@gmail.com>
2023-12-13 19:45:10 -0300

    Initial commit

---
+++ tests/Enums/CurrencyTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Mkioschi\Tests\Enums;
+
+use Mkioschi\Enums\Currency;
+use PHPUnit\Framework\TestCase;
+
+class CurrencyTest extends TestCase
+{
+    public function test_should_be_able_to_create_a_valid_country()
+    {
+        $brazilianReal = Currency::BRL;
+        $this->assertInstanceOf(Currency::class, $brazilianReal);
+        $this->assertEquals('BRL', $brazilianReal->value);
+        $this->assertEquals('Brazilian real', $brazilianReal->label());
+    }
+}
